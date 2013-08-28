eae9cf0c42deb8e198b3f5ed6a612268fde99559
Philippe GONCALVES (ddatc-wma1-r7) <philippe.goncalves@paris.fr>
2013-08-28 17:00:50 +0200

    Nouvelle implementation du decorator permettant de prendre en compte les parametres dynamiques.
    Les accessors aux parametres dynamiques sont fournis sous la forme d'une list (l_dynamic_param_name_and_dynamoc_accessor) d'elements de la structure DynamicParamNameAndDynamicAccessor.
    Un l_dynamic_param_name_and_dynamoc_accessor vide indique qu'il n'y a pas de parametres dynamqiques.
    Chaque element de l_dynamic_param_name_and_dynamoc_accessor correspond a un identifiant d'element et d'une fonction accedant a la valeur de l'element.
    
    Ce decorator doit remplacer le decorator @number_function_parameter ( @manage_ssh ) dans la mesure ou il reprend a focntionnalite manage_ssh.

--- cloudmgrws/tools.py
+++ cloudmgrws/tools.py
@@ -4,8 +4,13 @@ from __future__ import with_statement
 import  app
 import 	sys
 import 	pkg_resources
-from    webob.exc               import HTTPConflict
+from    webob.exc               import HTTPConflict, HTTPOk
 import  functools
+
+from	collections		import namedtuple
+from	collections		import OrderedDict
+
+from  	cloudmgrws.ssh_tools	import	manage_ssh
 
 def catch_not_found( f ):
     @functools.wraps( f )
@@ -39,5 +44,92 @@ def number_function_parameter( nb = 0, function_params_accessors = [] ):
             if nb != len( function_params ):
                raise HTTPConflict()
             return f( topology_params, function_params, *args, **kwargs )
+        return wrapped
+    return wrapper
+
+
+DynamicParamNameAndDynamicAccessor = 					\
+    namedtuple( 'DynamicParamNameAndDynamicAccessor', [ 'param_name', 'param_accessor' ] )
+
+def dynamic_parameters(
+    l_dynamic_param_name_and_dynamic_accessor = [],
+    ):
+    def wrapper( f ):
+
+        assert								\
+            isinstance(							\
+                l_dynamic_param_name_and_dynamic_accessor,		\
+                list,							\
+            ), 'Big Bug'						\
+
+        for k_v in 							\
+            l_dynamic_param_name_and_dynamic_accessor:			\
+                assert							\
+                    isinstance(						\
+                        k_v,						\
+                        DynamicParamNameAndDynamicAccessor,		\
+                    ), 'Big Bug'					\
+
+        d_dynamic_param_name_and_dynamic_accessor =			\
+            OrderedDict(
+                l_dynamic_param_name_and_dynamic_accessor
+            )
+
+        @manage_ssh
+        @functools.wraps( f )
+        def wrapped( topology_params, function_params, ssh, response, *args, **kwargs ):
+
+            if len( function_params ) > len( d_dynamic_param_name_and_dynamic_accessor ):
+                raise HTTPConflict()
+
+            if len( d_dynamic_param_name_and_dynamic_accessor ) > 0:
+
+                l_valid_params			=			\
+                    d_dynamic_param_name_and_dynamic_accessor.values()[ 0 ](
+                        topology_params,
+                        function_params,
+                        ssh,
+                        response,
+                        *args,
+                        **kwargs
+                    )
+
+                if len( function_params ) > 0:
+
+                    # Check validity for dynamic param
+                    for i, param in enumerate( function_params ):
+
+                        if param not in l_valid_params:
+
+                            # error on invalid dynamic param
+                            # dealed on ssh_tools
+                            raise HTTPConflict()
+
+                        if i + 1 < len( d_dynamic_param_name_and_dynamic_accessor ):
+                            l_valid_params              =                       \
+                                 d_dynamic_param_name_and_dynamic_accessor.values()[ i + 1 ](
+                                     topology_params,
+                                     function_params,
+                                     ssh,
+                                     response,
+                                     *args,
+                                     **kwargs
+                                )
+
+                if len( function_params ) < len( d_dynamic_param_name_and_dynamic_accessor ):
+
+                    e                           = HTTPOk()
+                    e.next                      =                               \
+                       {
+                           d_dynamic_param_name_and_dynamic_accessor.keys()[
+                               len( function_params )
+                           ]:                                                   \
+                               l_valid_params
+                       }
+
+                    raise e
+
+            return f( topology_params, function_params, ssh, response, *args, **kwargs )
+
         return wrapped
     return wrapper
